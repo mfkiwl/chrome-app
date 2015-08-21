f7f3ecce6fa9549896f17b46716661f17c21a3f8
Michaël Zasso <mic.besace@gmail.com>
2015-08-21 16:45:45 +0200

    prepare for new communication system

--- spectrophotometer/app/background.js
+++ spectrophotometer/app/background.js
@@ -1,10 +1,10 @@
 chrome.app.runtime.onLaunched.addListener(function() {
     chrome.app.window.create('index.html', {
-        'id':"",
+        id: '',
         singleton: true,
-        'outerBounds': {
-            'width': 1280,
-            'height': 720
+        outerBounds: {
+            width: 1280,
+            height: 720
         }
     });
 });
